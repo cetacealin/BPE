0abe0b9b5990ed57aebbb5ee6fddf73967a00f41
cclin <kuanyu0111@gmail.com>
2018-07-30 17:13:26 -0700

    list overlap

---
+++ exercise10.py
@@ -0,0 +1,24 @@
+'''This week’s exercise is going to be revisiting an old exercise (see Exercise 5), except require the solution in a different way.
+
+Take two lists, say for example these two:
+
+	a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+	b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+and write a program that returns a list that contains only the elements that are common between the lists (without duplicates). Make sure your program works on two lists of different sizes. Write this in one line of Python using at least one list comprehension. (Hint: Remember list comprehensions from Exercise 7).'''
+
+new_list = []
+
+def compare_lists(ls1, ls2):
+
+    for i in range(len(ls1)):
+        for k in range(len(ls2)):
+            if ls1[i] == ls2[k]:
+                new_list.append(ls1[i])
+    return new_list
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+print(compare_lists(a, b))
+
+
